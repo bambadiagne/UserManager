02dd16a0a52336d29a5ca9907e4cb86b52361d02
Ahmadou Bamba Diagne <diagne.bambahmadou@gmail.com>
2020-12-28 20:22:00 +0000

    Update user view by api model

--- usermanager/users/views.py
+++ usermanager/users/views.py
@@ -1,10 +1,10 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from .models import Utilisateur
-from .forms import UtilisateurForm
+from django.shortcuts import render,redirect
 import datetime
 from api.models import Seller,Client
+from api.utils import is_seller_or_client,user_form_validation
+from rest_framework.authtoken.models import Token
 
 def afficher_vendeurs(request):
     vendeurs = Seller.objects.all()
@@ -14,59 +14,48 @@ def afficher_vendeurs(request):
 
 def afficher_clients(request):
 
-    if(request.method == 'POST'):
-        user_form = UtilisateurForm(request.POST)
-        if(user_form.is_valid()):
-
-            date_birth = [int(i) for i in request.POST.get('date').split("/")]
-
-            Utilisateur.objects.create(
-                nom=user_form.cleaned_data['nom'],
-                prenom=user_form.cleaned_data['prenom'],
-                identifiant=user_form.cleaned_data["identifiant"],
-                email=user_form.cleaned_data['email'],
-                date_naissance=datetime.date(
-                    date_birth[2], date_birth[1], date_birth[0]),
-                mot_de_passe=user_form.cleaned_data['mot_de_passe'],
-                profile=user_form.cleaned_data['profile'],
-            )
-            return HttpResponseRedirect("/users/clients")
-
-        else:
-            return render(request, "users/ajouter_utilisateur.html", {"form": user_form})
     clients = Client.objects.all()
     clients=[User.objects.filter(id=vendeur.user_id).first() for vendeur in clients]
     return render(request, "users/clients.html", {'clients': clients})
 
 
 def supprimer_utilisateur(request, id):
-    client = Utilisateur.objects.get(identifiant=id).delete()
-    return render(request,'users/clients.html')
-    return render(request, 'users/clients')
+    
+    user=is_seller_or_client(id)
+    user[1].delete()
+    User.objects.filter(id=user[1].user_id).first().delete()      
+    user_token=Token.objects.filter(user_id=user[1].user_id).first()
+
+    if(user_token):
+        user_token.delete()
+    
+    return redirect('/users/clients')
+   
 def confirmation_suppression(request,id):
         return render(request,'users/confirmation.html',{'id':id})
 def modifier_utilisateur(request, id):
-    client = Utilisateur.objects.get(identifiant=id)
-
+    user = User.objects.get(id=id)
+    
     if(request.method == 'POST'):
-        user_form = UtilisateurForm(request.POST)
-        if(user_form.is_valid()):
-            date_birth = [int(i) for i in request.POST.get('date').split("/")]
-             
-            Utilisateur.objects.filter(identifiant=id).update(nom=user_form.cleaned_data['nom'],
-                                                     prenom=user_form.cleaned_data['prenom'],
-                                                     identifiant=user_form.cleaned_data["identifiant"],
-                                                     email=user_form.cleaned_data['email'],
-                                                     date_naissance=datetime.date(
-                                                         date_birth[2], date_birth[1], date_birth[0]),
-                                                     mot_de_passe=user_form.cleaned_data['mot_de_passe'],
-                                                     profile=user_form.cleaned_data['profile'],
-                                                     )
-            return HttpResponseRedirect("/users/clients")
-        else:
-            user_form=UtilisateurForm()
-            render(request, 'users/modifier_utilisateur.html', {'id': id, 'form': user_form})     
-    return render(request, 'users/modifier_utilisateur.html', {'id': id, 'client': client})
+        user_form=user_form_validation(request)
+        if(user_form[0] and user.username!=user_form[1]["username"]):
+            User.objects.filter(id=id).update(password=make_password(user_form[1]["mot_de_passe"]),username=user_form[1]["username"], last_name=user_form[1]["nom"],
+                                    email=user_form[1]["email"], first_name=user_form[1]["prenom"])    
+            if(request.POST.get('profile')=='seller'):        
+                
+                seller = Seller(user=user, date_naissance=datetime.date(
+                            user_form[1]["date_birth"][2], user_form[1]["date_birth"][1], user_form[1]["date_birth"][0]))
+                seller.save()
+                return render(request, "users/vendeurs.html")
+            elif(request.POST.get('profile') == "client"):
+                client = Client(user=user, date_naissance=datetime.date(
+                            user_form[1]["date_birth"][2], user_form[1]["date_birth"][1], user_form[1]["date_birth"][0]))
+                client.save()
+                return render(request, "users/clients.html")
+        return             
+        
+          
+    return render(request, 'users/modifier_utilisateur.html', {'id': id, 'client': user})
 
 
 def ajouter_utilisateur(request):
